ec33f0971095f55d0f70891f140c6c4fd2b3b6c6
DaveVodrazka <dave.vodrazka@gmail.com>
2025-12-16 12:47:33 +0100

    fix: add google calendar link

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    service: {
-      entry: 'astro/assets/services/noop',
-    },
-  },
   vite: {
     plugins: [tailwindcss()]
   }
