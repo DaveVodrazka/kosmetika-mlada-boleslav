cabe754538207d5aaf25a340b17cb2788301a8ba
DaveVodrazka <dave.vodrazka@gmail.com>
2025-12-16 14:55:12 +0100

    ci: fix workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, squooshImageService } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: squooshImageService(),
+  },
   vite: {
     plugins: [tailwindcss()]
   }
